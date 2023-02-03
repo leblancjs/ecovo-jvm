62d44588d1dee91846386c88e2b3c3c51683d886
Joel Leblanc <leblanc.js@icloud.com>
2023-02-02 19:52:45 -0500

    Added a shell project generated with the Spring Initializer for the trip-search-service.

--- README.md
+++ README.md
@@ -3,3 +3,4 @@ A JVM based version of Ecovo.
 
 ## Services
 * [User Service](user-service/README.md)
+* [Trip Search Service](trip-search-service/README.md)
---
+++ trip-search-service/src/main/java/com/solutionsjsleblanc/ecovo/tripsearchservice/TripSearchServiceApplication.java
@@ -0,0 +1,11 @@
+package com.solutionsjsleblanc.ecovo.tripsearchservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripSearchServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TripSearchServiceApplication.class, args);
+    }
+}
